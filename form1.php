49a64cd1744e8b5ba35f8c052dd7286244ea5682
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-05-24 16:00:50 +0200

    php

---
+++ form1.php
@@ -0,0 +1,9 @@
+<?php
+// if($_SERVER['REQUEST_METHOD']==='post'){
+    $name=$_POST['name'];
+    $marks=$_POST['marks'];
+    $subject=$_POST['subject'];
+    // a simple echo
+    echo "student info :</br> name: ".$name." </br> subject: ".$subject."</br> marks: ".$marks;
+// }
+?>
