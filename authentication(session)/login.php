f61c813779e47dedccb480621a7aa20c11bf9eda
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-06-23 00:11:34 +0200

    session with authentication

--- authentication(session)/login.php
+++ authentication(session)/login.php
@@ -1,38 +1,43 @@
 <?php
-require ("includes/db.php");
+require("includes/db.php");
 session_start();
-if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $username=$_POST['username'];
-    $password=$_POST['password'];
-    $stmt=$conn->prepare("SELECT * FROM sessions WHERE password = ? ");
-    $stmt->bind_param("s",$password);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Prepare the statement to select user based on username and password
+    $stmt = $conn->prepare("SELECT * FROM sessions WHERE username = ? AND password = ?");
+    $stmt->bind_param("ss", $username, $password);
     $stmt->execute();
-    if($stmt->num_rows == 1){
-        $row=$stmt->fetch();
-        $id=$row["id"];
-        $_SESSION["user_id"]=$id;
-      header("Location : welcome.php");
-    }else{
-        header("Location : register.php");
+    $result = $stmt->get_result();
+
+    if ($result->num_rows) {
+        $row = $result->fetch_assoc();
+        $id = $row["id"];
+        $_SESSION["user_id"] = $id;
+        header("Location: welcome.php");
+    } else {
+        header("Location: register.php");
     }
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>login</title>
-  </head>
-  <body>
-  <form action="login.php" method="post">
-    <p>username :</p>
-   <input type="text" id="username" name="username">
-<br>
-<p>password :</p>
-<input type="text" id="password" name="password">
-<br>
-<input type="submit">
+</head>
+<body>
+<form action="login.php" method="post">
+    <p>username:</p>
+    <input type="text" id="username" name="username">
+    <br>
+    <p>password:</p>
+    <input type="password" id="password" name="password">
+    <br>
+    <input type="submit">
 </form>
-  </body>
+</body>
 </html>
--- authentication(session)/welcome.php
+++ authentication(session)/welcome.php
@@ -3,6 +3,7 @@ session_start();
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
